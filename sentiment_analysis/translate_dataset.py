ecf8858c774ed4a0862d6aed58f58615909840b8
meteorgas <musayevresad23@gmail.com>
2025-03-31 19:08:34 +0300

    Translate hotel review dataset

--- sentiment_analysis/translate_dataset.py
+++ sentiment_analysis/translate_dataset.py
@@ -1,17 +1,20 @@
 import pandas as pd
+import asyncio
 from googletrans import Translator
 
-# Load dataset
 df = pd.read_csv("../review_dataset/chennai_reviews.csv")
-
-# Create a copy of the first 100 rows
-df_subset = df.head(100).copy()
-
-# Translate only this subset
 translator = Translator()
-df_subset["Review_Text_TR"] = df_subset["Review_Text"].apply(
-    lambda x: translator.translate(str(x), src="en", dest="tr").text
-)
 
-# Save only the translated 100 rows
-df_subset.to_csv("translated_first_1001.csv", index=False)
+async def translate_text(text):
+    try:
+        return (await translator.translate(str(text), src='en', dest='tr')).text
+    except:
+        return ""
+
+async def main():
+    tasks = [translate_text(text) for text in df["Review_Text"].astype(str)]
+    translated = await asyncio.gather(*tasks)
+    df["Review_Text_TR"] = translated
+    df.to_csv("../review_dataset/translated_chennai_reviews.csv", index=False)
+
+asyncio.run(main())
