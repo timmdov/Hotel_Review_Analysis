c201f046df10c1cef99e1c27229b94d7c9a2a44e
meteorgas <musayevresad23@gmail.com>
2025-04-15 00:45:37 +0300

    Add filter_turkish_reviews.py

--- ML_SentimentAnalysis.V1/dataset_operations/translate_dataset.py
+++ ML_SentimentAnalysis.V1/dataset_operations/translate_dataset.py
@@ -2,7 +2,7 @@ import pandas as pd
 import asyncio
 from googletrans import Translator
 
-df = pd.read_csv("../data/review_dataset/chennai_reviews.csv")
+df = pd.read_csv("../ML_SentimentAnalysis.V1/dataset/TripAdvisor Reviews Scraper.csv")
 translator = Translator()
 
 async def translate_text(text):
@@ -12,9 +12,9 @@ async def translate_text(text):
         return ""
 
 async def main():
-    tasks = [translate_text(text) for text in df["Review_Text"].astype(str)]
+    tasks = [translate_text(text) for text in df["Review"].astype(str)]
     translated = await asyncio.gather(*tasks)
     df["Review_Text_TR"] = translated
-    df.to_csv("../review_dataset/translated_chennai_reviews.csv", index=False)
+    df.to_csv("../ML_SentimentAnalysis.V1/dataset/Translated Reviews.csv", index=False)
 
 asyncio.run(main())
