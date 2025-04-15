a0336a1fb36d20e1ec34148458e2a05fba1cb004
meteorgas <musayevresad23@gmail.com>
2025-04-15 12:26:18 +0300

    Lemmatization

--- ML_SentimentAnalysis/config.py
+++ ML_SentimentAnalysis/config.py
@@ -4,6 +4,7 @@ TURKISH_ONLY_PATH = "../dataset/tripadvisor_tr_reviews.csv"
 CLEANED_PATH = "../dataset/tripadvisor_cleaned_reviews.csv"
 NORMALIZED_PATH = "../dataset/tripadvisor_normalized_reviews.csv"
 NO_STOP_WORDS_PATH = "../dataset/tripadvisor_cleaned_nostopwords.csv"
+LEMMATIZED_PATH = "../dataset/tripadvisor_lemmatized_reviews.csv"
 COLUMNS_TO_KEEP = ["HotelName", "Review", "Rating"]
 COLUMN_TO_DETECT_LANGUAGE = "Review"
 LANGUAGE_TO_FILTER = "tr"
